ab7233db445034c3bffc1efbda34824f36f02c03
Omar Cherni <89669057+ocherni@users.noreply.github.com>
2022-10-28 01:55:56 +0100

    Update crack_level_1.py

--- level_1/crack_level_1.py
+++ level_1/crack_level_1.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
 
-
-"""this module will be used for voting 4096 times
-    at the website provided"""
-import urllib
-from bs4 import BeautifulSoup
+# import modules
 import requests
+import sys
 
-failed = 0
+# level 1
 
-url = "http://158.69.76.135/level1.php"
-for i in range(0, 4096):
-    session = requests.session()
-    get_key_r = session.get(url)
-    content = get_key_r.content
-    soup = BeautifulSoup(content, 'html.parser')
-    inputs = soup.find_all('input')
-    for line in inputs:
-        if line.get('name') == "key":
-            key = line.get('value')
-    
-    payload = {'id': '496', 'key': key, 'holdthedoor': 'submit'}
-    r = session.post(url, data=payload)
-    if r.status_code != 200:
-        print("failed to post {}th request".format(i))
-        failed += 1
-    if i % 10 == 0:
-        print("posted 10 requests to server")
-    del session
-print("Failed {} number of requests".format(failed))
+url = 'http://158.69.76.135/level1.php'
+cookie_list = []
+id = 2500
+times = 1024
+
+for i in range(times):
+    response_get = requests.post(url, data={'id': id, 'holdthedoor': 'Enviar'})
+    key_form = response_get.headers['set-cookie']
+    cookie_list = key_form.split(';')
+    key_form = cookie_list[0][12:]
+    print("{}, {}".format(key_form, type(key_form)))
+    response_post = requests.post(
+    url,
+    data={'id': id, 'holdthedoor': 'Enviar', 'key': key_form},
+    cookies={'holdTheDoor': response_get.cookies}
+    )
+    print(response_post.headers)
+    print("Status: {} of 100".format(i + 1))
